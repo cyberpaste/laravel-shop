cdd712b4d6089c159fadb8cde9af0ec6c5e253e7
cyberpaste <proovit08@gmail.com>
2018-09-11 22:43:38 +0300

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
+
+Route::get('/', 'Index@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth', 'roles'], 'roles' => 'Admin'], function () {
+    Route::get('admin', 'Admin\AdminController@index');
+    Route::resource('admin/roles', 'Admin\RolesController');
+    Route::resource('admin/permissions', 'Admin\PermissionsController');
+    Route::resource('admin/users', 'Admin\UsersController');
+    Route::resource('admin/pages', 'Admin\PagesController');
+    Route::resource('admin/activitylogs', 'Admin\ActivityLogsController')->only([
+        'index', 'show', 'destroy'
+    ]);
+    Route::resource('admin/settings', 'Admin\SettingsController');
+    Route::resource('admin/categories', 'Admin\\CategoriesController');
+    Route::get('admin/generator', ['uses' => '\Appzcoder\LaravelAdmin\Controllers\ProcessController@getGenerator']);
+    Route::post('admin/generator', ['uses' => '\Appzcoder\LaravelAdmin\Controllers\ProcessController@postGenerator']);
+    Route::resource('admin/chunk', 'Admin\\ChunkController');
+    Route::resource('admin/currency', 'Admin\\CurrencyController');
+});
